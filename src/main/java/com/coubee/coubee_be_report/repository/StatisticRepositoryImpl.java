8fbc0ed9132a41d6b67ae86a8b85e1b730d32c28
jangseungwon08 <jangseungwon9819@gmail.com>
2025-08-13 18:20:39 +0900

    주별 일별 월별 매출 통계 구현

--- src/main/java/com/coubee/coubee_be_report/repository/StatisticRepositoryImpl.java
+++ src/main/java/com/coubee/coubee_be_report/repository/StatisticRepositoryImpl.java
@@ -31,7 +31,7 @@ public class StatisticRepositoryImpl implements StatisticRepository{
     @Override
     public Long weeklyStatisticSql(Long storeId, LocalDateTime startWeek, LocalDateTime endWeek) {
         String sql = "SELECT COALESCE(SUM(o.total_amount),0) AS daily_amount " +
-                "FROM orders o WHERE o.store_id = ? AND o.updated_at >= ? AND o.updated_at < ?  AND o.status = ?";
+                "FROM coubee_order.orders o WHERE o.store_id = ? AND o.updated_at >= ? AND o.updated_at < ?  AND o.status = ?";
         return jdbcTemplate.queryForObject(
                 sql,
                 Long.class,
@@ -46,7 +46,7 @@ public class StatisticRepositoryImpl implements StatisticRepository{
     @Override
     public Long monthlyStatisticSql(Long storeId,LocalDateTime startMonth, LocalDateTime endMonth) {
         String sql = "SELECT COALESCE(SUM(o.total_amount),0) AS daily_amount " +
-                "FROM orders o WHERE o.store_id = ? AND o.updated_at >= ? AND o.updated_at < ?  AND o.status = ?";
+                "FROM coubee_order.orders o WHERE o.store_id = ? AND o.updated_at >= ? AND o.updated_at < ?  AND o.status = ?";
         return jdbcTemplate.queryForObject(
                 sql,
                 Long.class,
