b9c59ac1c82229b0737b6e9f50fbf87ad9a7d30d
jangseungwon08 <jangseungwon9819@gmail.com>
2025-08-13 15:52:43 +0900

    refactor-orderStatus대분자 변경-[BTS-104]

--- src/main/java/com/coubee/coubee_be_report/constant/OrderStatusConstant.java
+++ src/main/java/com/coubee/coubee_be_report/constant/OrderStatusConstant.java
@@ -2,9 +2,9 @@ package com.coubee.coubee_be_report.constant;
 
 public class OrderStatusConstant {
     private OrderStatusConstant() {}
-    public static final String Pending = "Pending";
-    public static final String Paid = "Paid";
-    public static final String Preparing = "Preparing";
-    public static final String Prepared = "Prepared";
-    public static final String Received = "Received";
+    public static final String Pending = "PENDING";
+    public static final String Paid = "PAID";
+    public static final String Preparing = "PREPARING";
+    public static final String Prepared = "PREPARED";
+    public static final String Received = "RECEIVED";
 }
