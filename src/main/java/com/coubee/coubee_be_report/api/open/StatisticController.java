10d474a0d8d6fe1a1c827097f5e3331e5b7d10e1
jangseungwon08 <jangseungwon9819@gmail.com>
2025-08-13 14:16:03 +0900

    개발환경 세팅

--- src/main/java/com/coubee/coubee_be_report/api/open/StatisticController.java
+++ src/main/java/com/coubee/coubee_be_report/api/open/StatisticController.java
@@ -17,26 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/reports", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/reports/admin", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class StatisticController {
     private final StatisticService statisticService;
 
     @GetMapping(value = "/sales/daily/{storeId}")
     public ApiResponseDto<DailyStatisticDto> viewDailyStatistic(@RequestParam Long storeId){
-        String userId = GatewayRequestHeaderUtils.getUserName();
+        String userId = GatewayRequestHeaderUtils.getUserIdOrThrowException();
         DailyStatisticDto dailyStatistic = statisticService.dailyStatistic(storeId);
         return ApiResponseDto.createOk(dailyStatistic);
     }
     @GetMapping(value = "/sales/weekly/{storeId}")
     public ApiResponseDto<WeeklyStatisticDto> viewWeeklyStatistic(@RequestParam Long storeId){
-        String userId = GatewayRequestHeaderUtils.getUserName();
+        String userId = GatewayRequestHeaderUtils.getUserIdOrThrowException();
         WeeklyStatisticDto weeklyStatistic = statisticService.weeklyStatistic(storeId);
         return ApiResponseDto.createOk(weeklyStatistic);
     }
     @GetMapping(value = "/sales/monthly/{storeId}")
     public ApiResponseDto<MonthlyStatisticDto> viewMonthlyStatistic(@RequestParam Long storeId){
-        String userId = GatewayRequestHeaderUtils.getUserName();
+        String userId = GatewayRequestHeaderUtils.getUserIdOrThrowException();
         MonthlyStatisticDto monthlyStatistic = statisticService.monthlyStatistic(storeId);
         return ApiResponseDto.createOk(monthlyStatistic);
     }
